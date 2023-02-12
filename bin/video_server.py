dec614252bce9adac617bc60820b59369edb921b
jmcmah10 <jmcmah10@binghamton.edu>
2023-02-12 22:03:15 +0000

    Working on video_server

--- bin/video_server.py
+++ bin/video_server.py
@@ -4,10 +4,11 @@ from .proto_server import ProtoServer
 from lib.network.generated.Protobuf.wrapper_pb2 import WrappedMessage
 
 class VideoServer(ProtoServer):
-	def on_message(self, data, source): 
-		wrapper = WrappedMessage.FromString(data)
-		self.on_message(wrapper, source)
+	#def on_message(self, data, source): 
+	#	wrapper = WrappedMessage.FromString(data)
+	#	self.on_message(wrapper, source)
 
 	# Can override this in a subclass
 	def on_message(self, wrapper, source): 
-		print(f"Received a {wrapper.name} message from {source}")
+		if wrapper.name == "AdjustCamera":
+			command = AdjustCamera.FromString(wrapper.data)
