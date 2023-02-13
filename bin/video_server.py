7d0cd03da48da51b83536ffb0fe4c5cf2e6afecd
Daniel Nissim <dnissim1@binghamton.edu>
2023-02-13 14:53:18 -0500

    Changes to video server

--- bin/video_server.py
+++ bin/video_server.py
@@ -12,3 +12,14 @@ class VideoServer(ProtoServer):
 	def on_message(self, wrapper, source): 
 		if wrapper.name == "AdjustCamera":
 			command = AdjustCamera.FromString(wrapper.data)
+
+		if (camEnabled[AdjustCamera.id] == 0) && (AdjustCamera.isEnabled == True)
+			thread = camThread("Camera", list(serials.keys())[AdjustCamera.id])
+			thread.start()
+			cam_status(camID, True)
+			camEnabled[AdjustCamera.id] = 1
+		elif (camEnabled[AdjustCamera.id] == 1) && (AdjustCamera.isEnabled == False)
+			thread.raise_exception()
+			cam_status(camID, False)
+			camEnabled[AdjustCamera.id] = 0
+
