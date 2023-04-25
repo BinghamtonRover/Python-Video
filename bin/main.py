bb0078c8aca2daa8aefd83f78fa437d60c85ae5c
Levi Lesches <levilesches@gmail.com>
2023-04-25 06:18:30 -0400

    Cleanup

--- bin/main.py
+++ bin/main.py
@@ -9,29 +9,8 @@ from lib.thread import *
 from lib.server import VideoServer
 from network.generated import *
 
-def main(threads): 
-	# for camera in CameraName.values():
-	# 	# camera is an ID, 0-6
-	# 	if any(thread.camera_name == camera for thread in threads): 
-	# 		print(f"Has cam")
-	# 	else: print("Does not have cam")
-	# data = [
-	# 	CameraStatus(
-	# 		name=camera, 
-	# 		is_enabled=any(thread.camera_name == camera for thread in threads),
-	# 		resolution=constants.default_resolution[0]
-	# 	)
-	# 	for camera in CameraName.values()
-	# ]
-	# data_socket.listen()
-	# message = VideoData(cameras=data)
-	while True: pass
-
 if __name__ == '__main__':
 	print("Initializing...")
 	data_socket = VideoServer(port=8002)
-	# threads = get_threads(video_socket)
-	# if not threads: quit("No workable camera detected")
-	try: 
-		data_socket.listen()
+	try: data_socket.listen()
 	except KeyboardInterrupt: pass
