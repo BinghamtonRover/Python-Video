9124b2b0799bfa457ca89ade72da08df3d636e18
Levi Lesches <levilesches@gmail.com>
2023-05-24 17:48:38 -0400

    Using new `ServerThread`

--- bin/main.py
+++ bin/main.py
@@ -1,12 +1,8 @@
+from network import *
 from lib.server import VideoServer
 
 if __name__ == '__main__':
 	print("Initializing...")
-	data_socket = VideoServer(port=8002)
-	while True: 
-		try: data_socket.listen()
-		except KeyboardInterrupt: quit()
-		except OSError as error: 
-			# UDP error when a socket is closed during use. Ignore and try again.
-			if error.errno == 10054 or error.errno == 101: continue
-			else: raise error
+	socket = VideoServer(port=8002)
+	thread = ServerThread(socket)
+	ServerThread.startThreads([thread])
