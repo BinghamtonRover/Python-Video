eb811a3c00f5f37244d6d47de84c201826395086
Levi Lesches <levilesches@gmail.com>
2023-05-31 12:42:26 -0600

    Updated camera names

--- lib/constants.py
+++ lib/constants.py
@@ -4,12 +4,11 @@ from network.generated import *
 # 
 # This is HIGHLY dependent on the EXACT order of the USB ports.
 camera_names = {
-    0: CameraName.ROVER_REAR,
-    22: CameraName.AUTONOMY_DEPTH,
-    24: CameraName.ROVER_FRONT,
-    9: CameraName.SCIENCE_CAROUSEL,
-    2: CameraName.SCIENCE_VACUUM,
-    4: CameraName.SCIENCE_MICROSCOPE,
+	"/dev/realsense_rgb": CameraName.ROVER_FRONT,
+	"/dev/realsense_depth": CameraName.AUTONOMY_DEPTH,
+	"/dev/subsystem1": CameraName.SUBSYSTEM1,
+	"/dev/subsystem2": CameraName.SUBSYSTEM2,
+	"/dev/subsystem3": CameraName.SUBSYSTEM3,
 }
 
 def get_default_details(index): return CameraDetails(
